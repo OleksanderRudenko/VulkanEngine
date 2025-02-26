db283baae8c66baf5120e0996cd47199e67b0eb9
Oleksander <rudenkoolexandr@gmail.com>
2025-02-26 18:06:36 +0200

    premake added

---
+++ premake5.lua
@@ -0,0 +1,71 @@
+workspace "VulkanEngine"
+    configurations { "Debug", "Release" }
+    architecture "x64"
+
+project "VulkanEngine"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    location "build"
+
+    targetdir ("bin/" .. "%{cfg.buildcfg}")
+    objdir ("bin-int/" .. "%{cfg.buildcfg}")
+
+    files { "src/**.h", "src/**.cpp" }
+
+    includedirs {
+        "Libraries/GLFW/include",
+        "Libraries/GLM"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+-- Vulkan Detection
+local vulkanSDK = os.getenv("VULKAN_SDK")
+
+if vulkanSDK then
+    print("Vulkan SDK found: " .. vulkanSDK)
+    libdirs { vulkanSDK .. "/Lib" }
+    filter "configurations:Debug"
+        includedirs { vulkanSDK .. "/Include" }
+        links { "vulkan-1" }
+
+    filter "configurations:Release"
+        includedirs { vulkanSDK .. "/Include" }
+        links { "vulkan-1" }
+
+    defines { "HAS_VULKAN" }
+else
+    print("Vulkan SDK NOT found! Skipping Vulkan integration.")
+end
+
+glfwPath = path.getabsolute("Libraries/GLFW")
+
+-- build GLFW with CMake
+if not os.isfile(glfwPath .. "/build/CMakeCache.txt") then
+    print("Building GLFW using CMake...")
+
+    os.execute("mkdir \"" .. glfwPath .. "/build\"")
+
+    os.execute("cd \"" .. glfwPath .. "/build\" && cmake .. -G \"Visual Studio 17 2022\"")
+    os.execute("cd \"" .. glfwPath .. "/build\" && cmake --build . --config Debug")
+    os.execute("cd \"" .. glfwPath .. "/build\" && cmake --build . --config Release")
+end
+
+-- Link GLFW built libraries
+filter "configurations:Debug"
+    libdirs { glfwPath .. "/build/src/Debug" }
+    links { "glfw3" }
+
+filter "configurations:Release"
+    libdirs { glfwPath .. "/build/src/Release" }
+    links { "glfw3" }
---
+++ src/shaders/compile.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+if not defined VULKAN_SDK (
+    echo Vulkan SDK not found. Make sure it is installed and the environment variable is set.
+    exit /b 1
+)
+
+set GLSLC="%VULKAN_SDK%\Bin\glslc.exe"
+
+if not exist %GLSLC% (
+    echo glslc.exe not found in Vulkan SDK directory.
+    exit /b 1
+)
+
+%GLSLC% shader.vert -o vert.spv
+%GLSLC% shader.frag -o frag.spv
+
+echo Shader compilation completed.
+pause
